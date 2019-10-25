e1bcaae1d5a980cf10aecd1c6befb13af006ec1d
Dennis <Dennis@DESKTOP-G1VOPEL.fritz.box>
2019-10-25 11:32:46 +0200

    created simple hello world program

---
+++ TestProject/src/HelloRunner.java
@@ -0,0 +1,9 @@
+
+public class HelloRunner {
+
+	public static void main(String[] args) {
+		HelloWorld tester = new HelloWorld();
+		tester.sayHello();
+	}
+
+}
---
+++ TestProject/src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+	
+	public void sayHello() {
+		System.out.println("Hello World!");
+	}
+
+}
